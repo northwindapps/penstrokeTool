5f8d7e54c96a4ce20870b370470fdf7002b57193
yujin <northwindapps@gmail.com>
2025-01-08 21:35:56 +0900

    2strokes

--- readme.txt
+++ readme.txt
@@ -1,2 +1,36 @@
 use oneLine.py first,then use datasetUploadScript.py
 
+//reset dataset
+rm -rf ~/.cache/huggingface/datasets/*    
+
+
+1_0v
+2_0v
+3_0v
+4_0v
+6_0v
+6_2v
+7_1v
+9_0v
+10_0v
+12_0v
+13_1v
+
+0_1h
+3_1h
+3_2h
+3_3h
+4_1h
+4_2h
+6_1h
+7_0h
+7_2h
+8_0h
+13_0h
+17_0h
+17_1h
+
+11_1bs
+14_0bs
+15_0bs
+
---
+++ removeRow.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+# Replace 'data.json' with the path to your JSONL file
+file_path = '/Users/yano/Documents/penstrokeTool/data/test/test.jsonl'
+# Define the output file path
+output_file_path = '/Users/yano/Documents/penstrokeTool/data/train/no_vh.jsonl'
+
+# Ensure the output directory exists
+os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+# Open and load the JSONL file
+array = []
+with open(file_path, 'r') as file:
+    for line in file:
+        # Check if line does not contain "v" or "h"
+        try:
+            data = json.loads(line)  # Parse JSON
+            if data["sampleTag"] != "1v":
+            # if data["sampleTag"] != "bs" and data["sampleTag"] != "h" and data["sampleTag"] != "v":
+             array.append(data)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+
+# Write to the output JSONL file
+with open(output_file_path, 'w') as outfile:
+    for item in array:
+        outfile.write(json.dumps(item) + '\n')  # Write each object as a JSON line
+        print(item)  # Optional: print for debugging
